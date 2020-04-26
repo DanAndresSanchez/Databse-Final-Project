260b17485e4f906ef423af5ada604b7dd722210d
Dan Sanchez <sanchezd6@students.rowan.edu>
2020-04-25 22:09:38 -0400

    Products now have their own picture

--- database_project/src/components/ProductCard.js
+++ database_project/src/components/ProductCard.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Card, CardBody, CardTitle, CardImg} from "reactstrap";
 import StarRating from "./StarRating";
 import Button from "reactstrap/es/Button";
 
-const ProductCard = ({name, price, rating, image, amount}) => {
-    return(
-        <Card  style={{width: '18rem'}}>
-            <CardImg top src="https://images.unsplash.com/photo-1586695691862-bed442d489f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80" alt="..."/>
-            <CardBody>
-                <a href="#pablo" onClick={e => e.preventDefault()}>
-                    <div className="motto text-center">
-                        <CardTitle tag="h3">{ name }</CardTitle>
+class ProductCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state={
+            url: ''
+        }
+    }
+
+    async getPic(){
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: this.props.name },
+            headers: {
+                Authorization: 'Client-ID dtaTz_Cge-6eCOjxaNcyFR-H4-nX9fcSzkZfx8B6LfA'
+            }
+        });
+
+        this.setState({url: response.data.results[2].urls.regular}) ;
+    }
+
+    render(){
+        this.getPic();
+        return(
+            <Card  style={{width: '18rem'}}>
+                <CardImg top src={this.state.url} alt="..."/>
+                <CardBody>
+                    <a href="#pablo" onClick={e => e.preventDefault()}>
+                        <div className="motto text-center">
+                            <CardTitle tag="h3">{ this.props.name }</CardTitle>
+                        </div>
+                    </a>
+
+                    <StarRating rating={this.props.rating} amount={this.props.amount} name={this.props.name}/>
+                    <h5 className="card-description text-center">{ this.props.price }</h5>
+                    <div className={'motto text-center'}>
+                        <Button>Buy Now</Button>
                     </div>
-                </a>
+                </CardBody>
 
-                <StarRating rating={rating} amount={amount} name={name}/>
-                <h5 className="card-description text-center">{ price }</h5>
-                <div className={'motto text-center'}>
-                    <Button>Buy Now</Button>
-                </div>
-            </CardBody>
+            </Card>
+        );
+    }
 
-        </Card>
-    );
 }
 
 export default ProductCard;
